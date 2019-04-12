1f8fe4d067ab48d6013c2f755977d3f8e6d7cca3
cdlopez9 <42393035+cdlopez9@users.noreply.github.com>
2019-04-11 17:35:16 -0700

    Chords w scalability

--- Main.java
+++ Main.java
@@ -5,180 +5,222 @@ import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
-
-    static int state;
+    Chord chord = new Chord();
 
     public static void main(String[] args) {
-        Random random = new Random();
-        state = random.nextInt(7) + 1;
-
-        generateString(state);
+        Main object = new Main();
+        object.generateString(3);
     }
 
-    public static void generateString(int state) {
+    final class Chord {
+        private int state = 3;
+        private int root = 3;
+        private int third = 5;
+        private int fifth = 7;
+
+        public void playChord() {
+            playSound(root);
+            playSound(third);
+            playSound(fifth);
+            sleep();
+        }
+    }
+
+    public void generateString(int state) {
         Random random = new Random();
         int nextState;
+        System.out.println();
 
         switch (state) {
             case 1:
-                playC();
-                playE();
-                playG();
-                sleep();
+                chord.playChord();
                 break;
             case 2:
-                playD();
-                playF();
-                playA();
-                sleep();
+                chord.playChord();
                 nextState = random.nextInt(2) + 1;
                 if (nextState == 1) {
-                    generateString(3);
+                    assignState(3);
+                    generateString(chord.state);
                 } else if (nextState == 2) {
-                    generateString(5);
+                    assignState(5);
+                    generateString(chord.state);
                 } else {
                     error();
                 }
                 break;
             case 3:
-                playE();
-                playG();
-                playB();
-                sleep();
-                nextState = random.nextInt(2) + 1;
+                chord.playChord();
+                nextState = random.nextInt(3) + 1;
                 if (nextState == 1) {
-                    generateString(2);
+                    assignState(2);
+                    generateString(chord.state);
                 } else if (nextState == 2) {
-                    generateString(4);
+                    assignState(4);
+                    generateString(chord.state);
+                } else if (nextState == 3) {
+                    assignState(6);
+                    generateString(chord.state);
                 } else {
                     error();
                 }
                 break;
             case 4:
-                playF();
-                playA();
-                playC();
-                sleep();
+                chord.playChord();
                 nextState = random.nextInt(4) + 1;
                 if (nextState == 1) {
-                    generateString(1);
+                    assignState(1);
+                    generateString(chord.state);
                 } else if (nextState == 2) {
-                    generateString(2);
+                    assignState(2);
+                    generateString(chord.state);
                 } else if (nextState == 3) {
-                    generateString(3);
+                    assignState(3);
+                    generateString(chord.state);
                 } else if (nextState == 4) {
-                    generateString(5);
+                    assignState(5);
+                    generateString(chord.state);
                 } else {
                     error();
                 }
                 break;
             case 5:
-                playG();
-                playB();
-                playD();
-                sleep();
-                nextState = random.nextInt(2) + 1;
-                if (nextState == 1) {
-                    generateString(1);
-                } else if (nextState == 2) {
-                    generateString(6);
-                } else {
-                    error();
-                }
+                chord.playChord();
+                assignState(1);
+                generateString(chord.state);
                 break;
             case 6:
-                playA();
-                playC();
-                playE();
-                sleep();
-                nextState = random.nextInt(3) + 1;
+                chord.playChord();
+                nextState = random.nextInt(2) + 1;
                 if (nextState == 1) {
-                    generateString(2);
+                    assignState(2);
+                    generateString(chord.state);
                 } else if (nextState == 2) {
-                    generateString(4);
-                } else if (nextState == 3) {
-                    generateString(5);
+                    assignState(4);
+                    generateString(chord.state);
                 } else {
                     error();
                 }
                 break;
             case 7:
-                playB();
-                playD();
-                playF();
-                sleep();
+                chord.playChord();
                 nextState = random.nextInt(2) + 1;
-                if(nextState == 1){
-                    generateString(1);
-                } else if (nextState == 2){
-                    generateString(3);
-                } else{
+                if (nextState == 1) {
+                    assignState(1);
+                    generateString(chord.state);
+                } else if (nextState == 2) {
+                    assignState(3);
+                    generateString(chord.state);
+                } else {
                     error();
                 }
                 break;
         }
     }
 
-    static void playA(){
-        System.out.println("A");
-        String note = "A";
-        playSound(note);
-    }
-    static void playB(){
-        System.out.println("B");
-        String note = "B";
-        playSound(note);
-    }
-    static void playC(){
-        System.out.println("C");
-        String note = "C";
-        playSound(note);
-    }
-    static void playD(){
-        System.out.println("D");
-        String note = "D";
-        playSound(note);
-    }
-    static void playE(){
-        System.out.println("E");
-        String note = "E";
-        playSound(note);
-    }
-    static void playF(){
-        System.out.println("F");
-        String note = "F";
-        playSound(note);
-    }
-    static void playG(){
-        System.out.println("G");
-        String note = "G";
-        playSound(note);
+    void assignState(int state) {
+        chord.state = state;
+        chord.root = state;
+        if (state <= 5) {
+            chord.third = state + 2;
+        } else {
+            chord.third = state - 5;
+        }
+
+        if (state <= 3) {
+            chord.fifth = state + 4;
+        } else {
+            chord.fifth = state - 3;
+        }
     }
 
-    static void error(){
+    void playSound(int note) {
+        switch (note) {
+            case 1:
+                playC();
+                break;
+            case 2:
+                playD();
+                break;
+            case 3:
+                playE();
+                break;
+            case 4:
+                playF();
+                break;
+            case 5:
+                playG();
+                break;
+            case 6:
+                playA();
+                break;
+            case 7:
+                playB();
+                break;
+        }
+    }
+
+    void playA() {
+        System.out.println("A");
+        String note = "A";
+        playFile(note);
+    }
+
+    void playB() {
+        System.out.println("B");
+        String note = "B";
+        playFile(note);
+    }
+
+    void playC() {
+        System.out.println("C");
+        String note = "C";
+        playFile(note);
+    }
+
+    void playD() {
+        System.out.println("D");
+        String note = "D";
+        playFile(note);
+    }
+
+    void playE() {
+        System.out.println("E");
+        String note = "E";
+        playFile(note);
+    }
+
+    void playF() {
+        System.out.println("F");
+        String note = "F";
+        playFile(note);
+    }
+
+    void playG() {
+        System.out.println("G");
+        String note = "G";
+        playFile(note);
+    }
+
+    void error() {
         System.out.println("ERROR");
     }
 
-    static void sleep(){
+    void sleep() {
         try {
             TimeUnit.SECONDS.sleep(1);
-        } catch(InterruptedException e){
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 
-    static void playSound(String note){
+    void playFile(String note) {
         try {
             File soundFile = new File("C:\\Users\\Christian\\IdeaProjects\\BonusProject\\src\\" + note + ".wav");
             AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
             Clip clip = AudioSystem.getClip();
             clip.open(audioIn);
             clip.start();
-        } catch (IOException e){
-            e.printStackTrace();
-        } catch (LineUnavailableException e){
-            e.printStackTrace();
-        } catch (UnsupportedAudioFileException e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
