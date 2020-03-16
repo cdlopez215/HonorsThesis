3d51ce50b6370a1b322276a57953ff151a3c0c08
cdlopez9 <42393035+cdlopez9@users.noreply.github.com>
2020-03-15 19:27:57 -0700

    Add files via upload
    Updated second window showing chord relationships to accept user input

--- App.java
+++ App.java
@@ -6,120 +6,363 @@
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class App extends JFrame {
-    int FIRST_ROW_OFFSET;
-    int SECOND_ROW_OFFSET;
-    int THIRD_ROW_OFFSET;
-    int FOURTH_ROW_OFFSET;
-
-    int FIRST_COLUMN_OFFSET;
-    int SECOND_COLUMN_OFFSET;
-    int THIRD_COLUMN_OFFSET;
-    int FOURTH_COLUMN_OFFSET;
+public class App extends JPanel implements ActionListener {
 
     int OVAL_WIDTH = 75;
     int OVAL_HEIGHT = 75;
-    int STRING_ADJUST = OVAL_WIDTH / 2;
+
+    int ROW_ADJUST, COLUMN_ADJUST;
+    private JFrame frame;
+
+    int mostRecent = 0, backUp, lastSideUsed;
+
+    boolean canGoTo[][] = new boolean[7][7];
+    JButton jb0 = new JButton("I");
+    JButton jb1 = new JButton("ii");
+    JButton jb2 = new JButton("iii");
+    JButton jb3 = new JButton("IV");
+    JButton jb4 = new JButton("V");
+    JButton jb5 = new JButton("vi");
+    JButton jb6 = new JButton("vii");
+
+    JButton jb8 = new JButton("I");
+    JButton jb9 = new JButton("ii");
+    JButton jb10 = new JButton("iii");
+    JButton jb11 = new JButton("IV");
+    JButton jb12 = new JButton("V");
+    JButton jb13 = new JButton("vi");
+    JButton jb14 = new JButton("vii");
+
+    boolean isActive[] = {true, true, true, true, true, true, true};
 
     int chord;
 
-    public App() {
-        //createWindow("Thesis", 600, 600);
+    public void createAndShowGui() {
+        ROW_ADJUST = 600 / 8;
+        COLUMN_ADJUST = 400 / 6;
+
+        frame = new JFrame("Chord Frame");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        this.setLayout(null);
+        this.setForeground(Color.WHITE);
+        this.setBackground(Color.GRAY);
+        this.add(Box.createRigidArea(new Dimension(0, 10)));
+
+        //chords on left
+        jb0.setBackground(Color.BLACK);
+        jb0.setName("lc0");
+        jb0.setActionCommand("lc0");
+        jb0.addActionListener(this);
+
+        jb1.setBackground(Color.BLACK);
+        jb1.setName("lc1");
+        jb1.setActionCommand("lc1");
+        jb1.addActionListener(this);
+
+        jb2.setBackground(Color.BLACK);
+        jb2.setName("lc2");
+        jb2.setActionCommand("lc2");
+        jb2.addActionListener(this);
+
+        jb3.setBackground(Color.BLACK);
+        jb3.setName("lc3");
+        jb3.setActionCommand("lc3");
+        jb3.addActionListener(this);
+
+        jb4.setBackground(Color.BLACK);
+        jb4.setName("lc4");
+        jb4.setActionCommand("lc4");
+        jb4.addActionListener(this);
+
+        jb5.setBackground(Color.BLACK);
+        jb5.setName("lc5");
+        jb5.setActionCommand("lc5");
+        jb5.addActionListener(this);
+
+        jb6.setBackground(Color.BLACK);
+        jb6.setName("lc6");
+        jb6.setActionCommand("lc6");
+        jb6.addActionListener(this);
+
+        jb0.setBounds(COLUMN_ADJUST, ROW_ADJUST * 1 - (50), OVAL_WIDTH, OVAL_HEIGHT);
+        this.add(jb0);
+
+        jb1.setBounds(COLUMN_ADJUST, ROW_ADJUST * 2 - (50), OVAL_WIDTH, OVAL_HEIGHT);
+        this.add(jb1);
+
+        jb2.setBounds(COLUMN_ADJUST, ROW_ADJUST * 3 - (50), OVAL_WIDTH, OVAL_HEIGHT);
+        this.add(jb2);
+
+        jb3.setBounds(COLUMN_ADJUST, ROW_ADJUST * 4 - (50), OVAL_WIDTH, OVAL_HEIGHT);
+        this.add(jb3);
+
+        jb4.setBounds(COLUMN_ADJUST, ROW_ADJUST * 5 - (50), OVAL_WIDTH, OVAL_HEIGHT);
+        this.add(jb4);
+
+        jb5.setBounds(COLUMN_ADJUST, ROW_ADJUST * 6 - (50), OVAL_WIDTH, OVAL_HEIGHT);
+        this.add(jb5);
+
+        jb6.setBounds(COLUMN_ADJUST, ROW_ADJUST * 7 - (50), OVAL_WIDTH, OVAL_HEIGHT);
+        this.add(jb6);
+
+        //chords on right
+        jb8.setBackground(Color.BLACK);
+        jb8.setName("rc0");
+        jb8.setActionCommand("rc0");
+        jb8.addActionListener(this);
+
+        jb9.setBackground(Color.BLACK);
+        jb9.setName("rc1");
+        jb9.setActionCommand("rc1");
+        jb9.addActionListener(this);
+
+        jb10.setBackground(Color.BLACK);
+        jb10.setName("rc2");
+        jb10.setActionCommand("rc2");
+        jb10.addActionListener(this);
+
+        jb11.setBackground(Color.BLACK);
+        jb11.setName("rc3");
+        jb11.setActionCommand("rc3");
+        jb11.addActionListener(this);
+
+        jb12.setBackground(Color.BLACK);
+        jb12.setName("rc4");
+        jb12.setActionCommand("rc4");
+        jb12.addActionListener(this);
+
+        jb13.setBackground(Color.BLACK);
+        jb13.setName("rc5");
+        jb13.setActionCommand("rc5");
+        jb13.addActionListener(this);
+
+        jb14.setBackground(Color.BLACK);
+        jb14.setName("rc6");
+        jb14.setActionCommand("rc6");
+        jb14.addActionListener(this);
+
+        jb8.setBounds(COLUMN_ADJUST * 4, ROW_ADJUST * 1 - (50), OVAL_WIDTH, OVAL_HEIGHT);
+        this.add(jb8);
+
+        jb9.setBounds(COLUMN_ADJUST * 4, ROW_ADJUST * 2 - (50), OVAL_WIDTH, OVAL_HEIGHT);
+        this.add(jb9);
+
+        jb10.setBounds(COLUMN_ADJUST * 4, ROW_ADJUST * 3 - (50), OVAL_WIDTH, OVAL_HEIGHT);
+        this.add(jb10);
+
+        jb11.setBounds(COLUMN_ADJUST * 4, ROW_ADJUST * 4 - (50), OVAL_WIDTH, OVAL_HEIGHT);
+        this.add(jb11);
+
+        jb12.setBounds(COLUMN_ADJUST * 4, ROW_ADJUST * 5 - (50), OVAL_WIDTH, OVAL_HEIGHT);
+        this.add(jb12);
+
+        jb13.setBounds(COLUMN_ADJUST * 4, ROW_ADJUST * 6 - (50), OVAL_WIDTH, OVAL_HEIGHT);
+        this.add(jb13);
+
+        jb14.setBounds(COLUMN_ADJUST * 4, ROW_ADJUST * 7 - (50), OVAL_WIDTH, OVAL_HEIGHT);
+        this.add(jb14);
+
+        JButton singleChord = new JButton("Add/Remove chord");
+        singleChord.setBackground(Color.BLACK);
+        singleChord.setName("singleChord");
+        singleChord.setActionCommand("singleChord");
+        singleChord.addActionListener(this);
+
+        singleChord.setBounds(100, 560, 200, 25);
+        this.add(singleChord);
+
+        JButton doubleChord = new JButton("Add/Remove relationship");
+        doubleChord.setBackground(Color.BLACK);
+        doubleChord.setName("doubleChord");
+        doubleChord.setActionCommand("doubleChord");
+        doubleChord.addActionListener(this);
+
+        doubleChord.setBounds(100, 590, 200, 25);
+        this.add(doubleChord);
+
+        frame.add(this);
+        frame.setVisible(true);
+        frame.setResizable(false);
+        frame.setSize(400, 650);
     }
 
-    public void createWindow(String title, int width, int height) {
-        setTitle(title);
-        setVisible(true);
-        setSize(width, height);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object obj = e.getSource();
+        JButton jb = null;
+        String name = "";
 
-        FIRST_ROW_OFFSET = height / 5;
-        SECOND_ROW_OFFSET = height / 5 * 2;
-        THIRD_ROW_OFFSET = height / 5 * 3;
-        FOURTH_ROW_OFFSET = height / 5 * 4;
+        jb = (JButton) obj;
+        name = jb.getName();
 
-        FIRST_COLUMN_OFFSET = width / 5;
-        SECOND_COLUMN_OFFSET = width / 5 * 2;
-        THIRD_COLUMN_OFFSET = width / 5 * 3;
-        FOURTH_COLUMN_OFFSET = width / 5 * 4;
+        switch (name) {
+            case "lc0":
+                backUp = mostRecent;
+                mostRecent = 0;
+                lastSideUsed = 0;
+                break;
+            case "lc1":
+                backUp = mostRecent;
+                mostRecent = 1;
+                lastSideUsed = 0;
+                break;
+            case "lc2":
+                backUp = mostRecent;
+                mostRecent = 2;
+                lastSideUsed = 0;
+                break;
+            case "lc3":
+                backUp = mostRecent;
+                mostRecent = 3;
+                lastSideUsed = 0;
+                break;
+            case "lc4":
+                backUp = mostRecent;
+                mostRecent = 4;
+                lastSideUsed = 0;
+                break;
+            case "lc5":
+                backUp = mostRecent;
+                mostRecent = 5;
+                lastSideUsed = 0;
+                break;
+            case "lc6":
+                backUp = mostRecent;
+                mostRecent = 6;
+                lastSideUsed = 0;
+                break;
+            case "rc0":
+                backUp = mostRecent;
+                mostRecent = 0;
+                lastSideUsed = 1;
+                break;
+            case "rc1":
+                backUp = mostRecent;
+                mostRecent = 1;
+                lastSideUsed = 1;
+                break;
+            case "rc2":
+                backUp = mostRecent;
+                mostRecent = 2;
+                lastSideUsed = 1;
+                break;
+            case "rc3":
+                backUp = mostRecent;
+                mostRecent = 3;
+                lastSideUsed = 1;
+                break;
+            case "rc4":
+                backUp = mostRecent;
+                mostRecent = 4;
+                lastSideUsed = 1;
+                break;
+            case "rc5":
+                backUp = mostRecent;
+                mostRecent = 5;
+                lastSideUsed = 1;
+                break;
+            case "rc6":
+                backUp = mostRecent;
+                mostRecent = 6;
+                lastSideUsed = 1;
+                break;
+            case "singleChord":
+                updateColor();
+                if (isActive[mostRecent]) {
+                    isActive[mostRecent] = false;
+                } else {
+                    isActive[mostRecent] = true;
+                }
+                repaint();
+                break;
+            case "doubleChord":
+                if (lastSideUsed == 1) {
+                    boolean temp = canGoTo[backUp][mostRecent];
+                    if (temp) {
+                        canGoTo[backUp][mostRecent] = false;
+                    } else {
+                        canGoTo[backUp][mostRecent] = true;
+                    }
+                }
+                repaint();
+                break;
 
-        setTitle(title);
-        setVisible(true);
-        setSize(width, height);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        }
     }
 
-    public void paint(Graphics g) {
-
-        super.paint(g);
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
         g.setColor(Color.GRAY);
-        super.paint(g);
+        super.paintComponent(g);
 
-        g.setColor(Color.GRAY);
-        g.fillOval(SECOND_COLUMN_OFFSET, FIRST_ROW_OFFSET, OVAL_WIDTH, OVAL_HEIGHT);
-        g.fillOval(SECOND_COLUMN_OFFSET, SECOND_ROW_OFFSET, OVAL_WIDTH, OVAL_HEIGHT);
-        g.fillOval(FIRST_COLUMN_OFFSET, THIRD_ROW_OFFSET, OVAL_WIDTH, OVAL_HEIGHT);
-        g.fillOval(THIRD_COLUMN_OFFSET, THIRD_ROW_OFFSET, OVAL_WIDTH, OVAL_HEIGHT);
-        g.fillOval(FOURTH_COLUMN_OFFSET, THIRD_ROW_OFFSET, OVAL_WIDTH, OVAL_HEIGHT);
-        g.fillOval(SECOND_COLUMN_OFFSET, FOURTH_ROW_OFFSET, OVAL_WIDTH, OVAL_HEIGHT);
-        g.fillOval(FOURTH_COLUMN_OFFSET, FOURTH_ROW_OFFSET, OVAL_WIDTH, OVAL_HEIGHT);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(Color.BLACK);
+        g2.setStroke(new BasicStroke(3));
 
-        //should change to individual string variables, get width of each string and adjust accordingly
-        g.setColor(Color.WHITE);
-        g.setFont(new Font("Courier", Font.BOLD, 40));
-        g.drawString("ii", SECOND_COLUMN_OFFSET + STRING_ADJUST - 10, FIRST_ROW_OFFSET + STRING_ADJUST + 12);
-        g.drawString("vi", SECOND_COLUMN_OFFSET + STRING_ADJUST - 14, SECOND_ROW_OFFSET + STRING_ADJUST + 12);
-        g.drawString("iii", FIRST_COLUMN_OFFSET + STRING_ADJUST - 13, THIRD_ROW_OFFSET + STRING_ADJUST + 12);
-        g.drawString("V", THIRD_COLUMN_OFFSET + STRING_ADJUST - 11, THIRD_ROW_OFFSET + STRING_ADJUST + 12);
-        g.drawString("vii", FOURTH_COLUMN_OFFSET + STRING_ADJUST - 17, THIRD_ROW_OFFSET + STRING_ADJUST + 12);
-        g.drawString("IV", SECOND_COLUMN_OFFSET + STRING_ADJUST - 14, FOURTH_ROW_OFFSET + STRING_ADJUST + 12);
-        g.drawString("I", FOURTH_COLUMN_OFFSET + STRING_ADJUST - 5, FOURTH_ROW_OFFSET + STRING_ADJUST + 12);
+        //lines
+        for (int leftChord = 0; leftChord < 7; leftChord++) {
+            for (int rightChord = 0; rightChord < 7; rightChord++) {
+                if (canGoTo[leftChord][rightChord]) {
+                    g2.drawLine(COLUMN_ADJUST + (OVAL_WIDTH), ROW_ADJUST * (leftChord + 1) - 25, COLUMN_ADJUST * 5 - (OVAL_WIDTH - 10), ROW_ADJUST * (rightChord + 1) - 25);
+                }
+            }
+        }
+    }
 
-        g.setColor(Color.GRAY);
-        g.drawLine(SECOND_COLUMN_OFFSET + OVAL_WIDTH / 2, FIRST_ROW_OFFSET + OVAL_HEIGHT, THIRD_COLUMN_OFFSET + OVAL_WIDTH / 2, THIRD_ROW_OFFSET);
-        g.drawLine(SECOND_COLUMN_OFFSET + OVAL_WIDTH / 2, FIRST_ROW_OFFSET + OVAL_HEIGHT, FOURTH_COLUMN_OFFSET + OVAL_WIDTH / 2, THIRD_ROW_OFFSET);
-        g.drawLine(SECOND_COLUMN_OFFSET + OVAL_WIDTH / 2, FIRST_ROW_OFFSET + OVAL_HEIGHT, FIRST_COLUMN_OFFSET + OVAL_WIDTH / 2, THIRD_ROW_OFFSET);
-        g.drawLine(SECOND_COLUMN_OFFSET + OVAL_WIDTH / 2, FIRST_ROW_OFFSET + OVAL_HEIGHT, SECOND_COLUMN_OFFSET + OVAL_WIDTH / 2, SECOND_ROW_OFFSET);
-        g.drawLine(SECOND_COLUMN_OFFSET + OVAL_WIDTH / 2, FIRST_ROW_OFFSET + OVAL_HEIGHT, FIRST_COLUMN_OFFSET - OVAL_WIDTH / 2, THIRD_ROW_OFFSET + OVAL_HEIGHT / 2);
-        g.drawLine(FIRST_COLUMN_OFFSET - OVAL_WIDTH / 2, THIRD_ROW_OFFSET + OVAL_HEIGHT / 2, SECOND_COLUMN_OFFSET, FOURTH_ROW_OFFSET + OVAL_HEIGHT / 2);
-        g.drawLine(SECOND_COLUMN_OFFSET + OVAL_WIDTH / 2, SECOND_ROW_OFFSET + OVAL_HEIGHT, FIRST_COLUMN_OFFSET + OVAL_WIDTH / 2, THIRD_ROW_OFFSET);
-        g.drawLine(SECOND_COLUMN_OFFSET + OVAL_WIDTH / 2, SECOND_ROW_OFFSET + OVAL_HEIGHT, THIRD_COLUMN_OFFSET + OVAL_WIDTH / 2, THIRD_ROW_OFFSET);
-        g.drawLine(SECOND_COLUMN_OFFSET + OVAL_WIDTH / 2, SECOND_ROW_OFFSET + OVAL_HEIGHT, FOURTH_COLUMN_OFFSET + OVAL_WIDTH / 2, THIRD_ROW_OFFSET);
-        g.drawLine(SECOND_COLUMN_OFFSET + OVAL_WIDTH / 2, SECOND_ROW_OFFSET + OVAL_HEIGHT, SECOND_COLUMN_OFFSET + OVAL_WIDTH / 2, FOURTH_ROW_OFFSET);
-        g.drawLine(FIRST_COLUMN_OFFSET + OVAL_WIDTH / 2, THIRD_ROW_OFFSET + OVAL_HEIGHT, SECOND_COLUMN_OFFSET + OVAL_WIDTH / 2, FOURTH_ROW_OFFSET);
-        g.drawLine(THIRD_COLUMN_OFFSET + OVAL_WIDTH / 2, THIRD_ROW_OFFSET + OVAL_HEIGHT, SECOND_COLUMN_OFFSET + OVAL_WIDTH / 2, FOURTH_ROW_OFFSET);
-        g.drawLine(THIRD_COLUMN_OFFSET + OVAL_WIDTH / 2, THIRD_ROW_OFFSET + OVAL_HEIGHT, FOURTH_COLUMN_OFFSET + OVAL_WIDTH / 2, FOURTH_ROW_OFFSET);
-        g.drawLine(FOURTH_COLUMN_OFFSET + OVAL_WIDTH / 2, THIRD_ROW_OFFSET + OVAL_HEIGHT, FOURTH_COLUMN_OFFSET + OVAL_WIDTH / 2, FOURTH_ROW_OFFSET);
-        g.drawLine(SECOND_COLUMN_OFFSET + OVAL_WIDTH, FOURTH_ROW_OFFSET + OVAL_HEIGHT / 2, FOURTH_COLUMN_OFFSET, FOURTH_ROW_OFFSET + OVAL_HEIGHT / 2);
-
-        //Changes current chord to blue
-        g.setColor(Color.BLUE);
-        switch (chord) {
-            case 1:
-                g.fillOval(FOURTH_COLUMN_OFFSET, FOURTH_ROW_OFFSET, OVAL_WIDTH, OVAL_HEIGHT);
-                break;
-            case 2:
-                g.fillOval(SECOND_COLUMN_OFFSET, FIRST_ROW_OFFSET, OVAL_WIDTH, OVAL_HEIGHT);
-                break;
-            case 3:
-                g.fillOval(FIRST_COLUMN_OFFSET, THIRD_ROW_OFFSET, OVAL_WIDTH, OVAL_HEIGHT);
-                break;
-            case 4:
-                g.fillOval(SECOND_COLUMN_OFFSET, FOURTH_ROW_OFFSET, OVAL_WIDTH, OVAL_HEIGHT);
-                break;
-            case 5:
-                g.fillOval(THIRD_COLUMN_OFFSET, THIRD_ROW_OFFSET, OVAL_WIDTH, OVAL_HEIGHT);
-                break;
-            case 6:
-                g.fillOval(SECOND_COLUMN_OFFSET, SECOND_ROW_OFFSET, OVAL_WIDTH, OVAL_HEIGHT);
-                break;
-            case 7:
-                g.fillOval(FOURTH_COLUMN_OFFSET, THIRD_ROW_OFFSET, OVAL_WIDTH, OVAL_HEIGHT);
-                break;
-            default:
-                break;
+    void updateColor() {
+        if(lastSideUsed == 0){
+            switch(mostRecent){
+                case 0:
+                    jb0.setBackground(Color.GRAY);
+                    break;
+                case 1:
+                    jb1.setBackground(Color.GRAY);
+                    break;
+                case 2:
+                    jb2.setBackground(Color.GRAY);
+                    break;
+                case 3:
+                    jb3.setBackground(Color.GRAY);
+                    break;
+                case 4:
+                    jb4.setBackground(Color.GRAY);
+                    break;
+                case 5:
+                    jb5.setBackground(Color.GRAY);
+                    break;
+                case 6:
+                    jb6.setBackground(Color.GRAY);
+                    break;
+            }
+        } else {
+            switch(mostRecent){
+                case 0:
+                    jb8.setBackground(Color.GRAY);
+                    break;
+                case 1:
+                    jb9.setBackground(Color.GRAY);
+                    break;
+                case 2:
+                    jb10.setBackground(Color.GRAY);
+                    break;
+                case 3:
+                    jb11.setBackground(Color.GRAY);
+                    break;
+                case 4:
+                    jb12.setBackground(Color.GRAY);
+                    break;
+                case 5:
+                    jb13.setBackground(Color.GRAY);
+                    break;
+                case 6:
+                    jb14.setBackground(Color.GRAY);
+                    break;
+            }
         }
     }
 }
--- Main.java
+++ Main.java
@@ -25,20 +25,16 @@ public class Main {
     Piano keyboard = new Piano();
 
     int currentNote, nextNote;
-    Chord[] chords = new Chord[8];
+    Chord[] chords = new Chord[7];
     long timeDelay = 1;
     Transposition transpose = new Transposition();
     int timeVar;
     int numPass = 0;
+    int[] roots = new int[100];
+    int numNotes = 0;
 
     List<Clip> players = new ArrayList<Clip>();
     ArrayList<List<Clip>> queue = new ArrayList<List<Clip>>();
-
-    public static void main(String[] args) {
-        Main object = new Main();
-        object.buildChords();
-        object.generateStringFromInput();
-    }
 
     /**
      * Contains information about notes in chord, which group of chords each is able to go to
@@ -56,6 +52,11 @@ public class Main {
 
         public void playChord() {
             players = new ArrayList<Clip>();
+
+            if(numNotes < 100) {
+                roots[numNotes] = root;
+                numNotes++;
+            }
 
             System.out.println("\n");
             playSound(root);
@@ -188,15 +189,16 @@ public class Main {
             default:
                 break;
         }
+
+        window.createAndShowGui();
+        for(int i = 0; i < 7; i++){
+            for(int j = 0; j < 7; j++){
+                window.canGoTo[i][j] = chords[i].canGoTo[j];
+            }
+        }
+        window.repaint();
     }
 
-    /**
-     * Methods for use with front end
-     * Untested
-     *
-     * @param originChord
-     * @param destChord
-     */
     public void addRule(Chord originChord, int destChord){
         boolean temp = chords[originChord.root].canGoTo[destChord];
 
@@ -208,26 +210,13 @@ public class Main {
         }
     }
 
-    public void removeRule(Chord originChord, int destChord){
-        boolean temp = chords[originChord.root].canGoTo[destChord];
-
-        if(temp) {
-            chords[originChord.root].canGoTo[destChord] = false;
-        } else {
-            //error
+    public void adjustRules(){
+        for(int i = 0; i < 7; i++){
+            chords[i].isActive = window.isActive[i];
+            for(int j = 0; j < 7; j++){
+                chords[i].canGoTo[j] = window.canGoTo[i][j];
+            }
         }
-    }
-
-    public void removeChord(Chord chord){
-        chords[chord.root].isActive = false;
-    }
-
-    public void addChord(Chord chord){
-        chords[chord.root].isActive = true;
-    }
-
-    public void changeTempo(int time){
-        timeDelay = time;
     }
 
     /**
@@ -242,9 +231,23 @@ public class Main {
      */
     public void generateStringFromInput(){
         while(true) {
+
+            roots = new int[100];
+            numNotes = 0;
+            queue = new ArrayList<>();
+            adjustChords();
+
             while (!Piano.entered) {
                 sleep();
             }
+
+            for(int i = 0; i < chords.length; i++) {
+                if(!Piano.chordBools[i]) {
+                    chords[i].isActive = false;
+                }
+            }
+
+            adjustRules();
 
             currentNote = Piano.notesEntered[0];
             nextNote = Piano.notesEntered[1];
@@ -274,20 +277,15 @@ public class Main {
             }
 
             for (int i = 0; i < Piano.numNotes; i++) {
+
                 currentNote = Piano.notesEntered[i];
 
                 if (i < Piano.numNotes - 1) {
                     nextNote = Piano.notesEntered[i + 1];
                 } else if (i == Piano.numNotes-1) {
-                    if (currentNote == 1 || currentNote == 3 || currentNote == 5) {
-                        currentChord = chords[0];
-                        currentChord.playChord();
-                        break;
-                    } else {
                         resolve();
                         System.out.print("\n\n***********");
                         i = Piano.numNotes;
-                    }
                 }
 
                 boolean nextBool = false;
@@ -313,7 +311,11 @@ public class Main {
                 i += skipCounter;
             }
 
-            playQueue();
+            if(numNotes < 100) {
+                playQueue();
+            } else {
+                System.out.println("No sequence possible");
+            }
 
             while(Piano.entered){
                 sleep();
@@ -326,6 +328,7 @@ public class Main {
      * Audio is broken up without it
      */
     public void playQueue(){
+        numNotes = 0;
         timeVar = Piano.timeVar / 60;
 
         for(int i = 0; i < queue.size(); i++){
@@ -344,18 +347,28 @@ public class Main {
         int rand = 0;
         boolean next = false;
 
-        while(!next) {
-            rand = random.nextInt(chords.length);
-            next = currentChord.canGoTo[rand];
-        }
-
-        currentChord = chords[rand];
-        currentNote = currentChord.root;
-        currentChord.playChord();
-        if(currentChord != chords[0]){
-            resolve();
+        if(currentChord.canGoTo[0] && chords[0].isActive){
+            currentChord = chords[0];
+            currentNote = currentChord.root;
+            currentChord.playChord();
         } else {
-            return;
+
+            while (!next) {
+
+                rand = random.nextInt(chords.length);
+                if (chords[rand].isActive) {
+                    next = currentChord.canGoTo[rand];
+                }
+            }
+
+            currentChord = chords[rand];
+            currentNote = currentChord.root;
+            currentChord.playChord();
+            if (currentChord != chords[0] && numNotes < 100) {
+                resolve();
+            } else {
+                return;
+            }
         }
     }
 
@@ -375,10 +388,9 @@ public class Main {
         int chordsPossible = 0;
         Chord[] nextValues = new Chord[7];
 
-        for(int i = 1; i < Piano.numNotes; i++){
+        for(int i = 1; i < 7; i++){
             boolean checkChord = current.canGoTo[i];
             if(checkChord){
-
                 if((chords[i].root == next || chords[i].third == next || chords[i].fifth == next) && chords[i].isActive){
                     returnValue = true;
                     nextValues[chordsPossible] = chords[i];
@@ -487,6 +499,10 @@ public class Main {
                     timeDelay = 500 / timeVar / numPass;
                 }
             } else{
+                window.chord = roots[numNotes];
+                numNotes++;
+                window.repaint();
+
                 numPass = findNumPassingTones(index);
                 if(numPass == 0){
                     timeDelay = 1000 / timeVar;
@@ -512,5 +528,11 @@ public class Main {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) {
+        Main object = new Main();
+        object.buildChords();
+        object.generateStringFromInput();
     }
 }
